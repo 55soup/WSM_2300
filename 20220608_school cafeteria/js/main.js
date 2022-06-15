5119f8059852b0b93239a014a78b62f603514a54
55soup <sunjoo2323@gmail.com>
2022-06-15 11:46:59 +0900

    html, css, js toggleMenu() 1

---
+++ 20220608_school cafeteria/js/main.js
@@ -0,0 +1,27 @@
+// show/hide menu: toggle menu
+const toggleMenu = function (toggleID, navListId){
+    //html -> js
+    const toggle = document.getElementById(toggleID);
+    const navList = document.getElementById(navListId);
+
+    const clickHandler  = function (){
+        //show/hidde menu: .show-menu
+        navList.classList.toggle('show-menu')
+    }
+
+    if(toggle && navList){
+        //toggle click
+        toggle.addEventListener('click', clickHandler);
+        //show/hide menu : show-menu
+    }
+}
+toggleMenu('nav-toggle', 'nav-list');
+// const say = () => { //화살표함수
+//     console.log("hello world4");
+// }
+// const say = function (){ //함수표현식
+//     console.log("hello world2");
+// }
+// function say(){ //함수선언식
+//     console.log("hello world");
+// }
